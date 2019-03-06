3ef69247841b8cc5a31987892f74105e26133106
Matthew Root <root.matthew@gmail.com>
2019-03-05 17:02:47 -0800

    Add basing routing with react-router-dom to allow switching been Characters, Events, and Series lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import MarvelList from './components/MarvelList';
 
@@ -6,9 +7,28 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const entities: string[] = ['characters', 'events', 'series'];
+    const listRoutes = entities.map(entity => {
+      const entityPath = '/'.concat(entity);
+      return (
+        <Route path={entityPath} render={() => <MarvelList type={entity} />} />
+      );
+    });
+
     return (
       <div className="App">
-        <MarvelList type="events" />
+        <ul>
+          <li>
+            <Link to="/characters">Characters</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/series">Series</Link>
+          </li>
+        </ul>
+        {listRoutes}
       </div>
     );
   }
