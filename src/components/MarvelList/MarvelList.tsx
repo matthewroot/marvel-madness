3905a0d8906ffe665b41c3fcdcb37a7e0030d06a
Matthew Root <root.matthew@gmail.com>
2019-03-08 17:33:51 -0800

    Add loading indicator to MarvelList component

--- src/components/MarvelList/MarvelList.tsx
+++ src/components/MarvelList/MarvelList.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { RingLoader } from 'react-spinners';
 
 import MarvelAPI from '../../utils/MarvelAPI';
 import MarvelCard from '../MarvelCard';
@@ -12,10 +13,12 @@ import {
 import './MarvelList.css';
 
 export default class MarvelList extends Component<any, any> {
+  loading: boolean;
   mounted: boolean;
 
   constructor(props: any) {
     super(props);
+    this.loading = true;
     this.mounted = false;
     this.state = { data: [] };
   }
@@ -25,6 +28,10 @@ export default class MarvelList extends Component<any, any> {
   async componentDidMount() {
     this.mounted = true;
     const data = await MarvelAPI.get({ entity: this.type });
+
+    if (data) {
+      this.loading = false;
+    }
 
     if (this.mounted) {
       this.setState({ data: data });
@@ -36,34 +43,44 @@ export default class MarvelList extends Component<any, any> {
   }
 
   render() {
-    return (
-      <div className="marvel-list">
-        {/* TODO: add loading indicator */}
-        <ul>
-          {this.state.data.map((data: Character | Event | Series) => {
-            let header;
+    const marvelList = (
+      <ul>
+        {this.state.data.map((data: Character | Event | Series) => {
+          let header;
 
-            if ((data as Character).name) {
-              header = (data as Character).name;
-            } else {
-              header = (data as Event | Series).title;
-            }
+          if ((data as Character).name) {
+            header = (data as Character).name;
+          } else {
+            header = (data as Event | Series).title;
+          }
 
-            return (
-              <li key={data.id}>
-                <Link to={`/${this.type}/${data.id}`} key={data.id}>
-                  <MarvelCard
-                    id={data.id}
-                    header={header}
-                    thumbnail={data.thumbnail}
-                    key={data.id}
-                  />
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+          return (
+            <li key={data.id}>
+              <Link to={`/${this.type}/${data.id}`} key={data.id}>
+                <MarvelCard
+                  id={data.id}
+                  header={header}
+                  thumbnail={data.thumbnail}
+                  key={data.id}
+                />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+
+    const loader = (
+      <div className="loading-container">
+        <div className="loading-item">
+          <RingLoader loading={this.loading} size={120} />
+          <p>Querying Cerebro</p>
+        </div>
       </div>
+    );
+
+    return (
+      <div className="marvel-list">{this.loading ? loader : marvelList}</div>
     );
   }
 }
