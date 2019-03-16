d998542a0c0bb3f2b7bdeb57a9e9d27a9abf6d0c
Matthew Root <root.matthew@gmail.com>
2019-03-15 21:55:35 -0700

    Adjust Grid alignment for MarvelEntityDetails and subcomponents

--- src/components/MarvelPlot/YearlyAppearancesPlot/YearlyAppearancesPlot.tsx
+++ src/components/MarvelPlot/YearlyAppearancesPlot/YearlyAppearancesPlot.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 
+import './YearlyAppearancesPlot.css';
+
 export default function YearlyAppearancesPlot({ plotData }: any) {
   return (
     <div className="yearly-appearances-plot">
       <h2>Yearly Appearances</h2>
-      <BarChart width={730} height={250} data={plotData}>
+      <BarChart width={750} height={250} data={plotData}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="year" />
         <YAxis />
